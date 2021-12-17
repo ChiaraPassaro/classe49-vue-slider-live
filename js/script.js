adc35e699f04a448420b8251560c0cd08dbf2eed
Chiara Passaro <chiarapassaro.wd@gmail.com>
2021-12-17 10:12:51 +0100

    bonus

--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
+// 1- al click su uno dei pallini mostrare l’immagine in posizione corrispondente
+// 2- applicare l’autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
+// 3- quando il mouse va in hover sullo slider, bloccare l’autoplay e farlo riprendere quando esce
+
 const app = new Vue(
   {
     el: '#app',
     data: {
       counter: null,
+      autoPlayInterval: null,
       images: [
         // 'image1.jpg',
         // 'image2.jpg',
         // 'image3.jpg',
         // 'image4.jpg',
+      
       ]
+    },
+    mounted() {
+
     },
     methods: {
       next: function () {
@@ -22,16 +31,52 @@ const app = new Vue(
         if (this.counter < 0) {
           this.counter = this.images.length - 1;
         }
+      },
+      changeImage: function (index) {
+        console.log(index);
+        this.counter = index;
+      },
+      stopAutoplay: function () {
+        console.log('stop');
+        clearInterval(this.autoPlayInterval);
+        this.autoPlayInterval = null;
+      },
+      startAutoplay: function () {
+        this.autoPlayInterval = setInterval(() => {
+          this.next();
+        }, 1000);
+      },
+      isActive: function (index) {
+        // (counter == index) ? 'active' : ''
+        // let variabile = (counter == index) ? 'active' : '';
+        if (this.counter == index) {
+          return 'active';
+        } 
+        return '';
+
       }
     },
     created() {
+      //simuliamo chiamata a server
+      // setTimeout(() => {
+      //   this.images = [
+      //     'image1.jpg',
+      //     'image2.jpg',
+      //     'image3.jpg',
+      //     'image4.jpg'
+      //   ];
+      // }, 1000);
+      
       this.images = [
-        'image1.jpg',
-        'image2.jpg',
-        'image3.jpg',
-        'image4.jpg'
-      ];
+          'image1.jpg',
+          'image2.jpg',
+          'image3.jpg',
+          'image4.jpg'
+        ];
+
       this.counter = 0;
+
+      this.startAutoplay();
     }
   }
 );
