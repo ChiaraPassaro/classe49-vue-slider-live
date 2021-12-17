c4f0044a0be55e5fdf99c061db9d7e6e4016dbe1
Chiara Passaro <chiarapassaro.wd@gmail.com>
2021-12-17 09:02:45 +0100

    first step

--- js/script.js
+++ js/script.js
@@ -22,26 +22,16 @@ const app = new Vue(
         if (this.counter < 0) {
           this.counter = this.images.length - 1;
         }
-      },
-      alertNextTot: function () {
-        setTimeout(function () { 
-          console.log('timeout');
-
-        }, 1000);
       }
     },
     created() {
-      console.log('created');
       this.images = [
         'image1.jpg',
         'image2.jpg',
         'image3.jpg',
         'image4.jpg'
       ];
-
       this.counter = 0;
-      
-      this.alertNextTot();
     }
   }
 );
